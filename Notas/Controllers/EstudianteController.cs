b3050b292d2b99f525fb6f02e8f295449bf27a58
Javier Abarca Jimenez <javier.abarca@ucr.ac.cr>
2020-07-03 22:10:37 -0600

    Base datos, modelos, controladores, servicio y un poco de estilo

---
+++ Notas/Controllers/EstudianteController.cs
@@ -0,0 +1,28 @@
+﻿using Notas.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Notas.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: Estudiante
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VerEstudiantes()
+        {
+            EstudianteService estudianteService = new EstudianteService();
+            return View(estudianteService.GetEstudiantes());
+        }
+
+
+
+
+    }
+}
---
+++ Notas/Services/EstudianteService.cs
@@ -0,0 +1,52 @@
+﻿using Notas.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Notas.Services
+{
+    public class EstudianteService
+    {
+        private SqlConnection con;
+        private void Connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["NotasDBEntities1"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        public List<EstudianteModel> GetEstudiantes() {
+            Connection();
+            string consulta =
+               "SELECT E.carne, E.nombre, E.apellido, E.apellido2 " +
+               "FROM Estudiante E " +
+               "ORDER BY E.carne ";
+
+            SqlCommand cmd = new SqlCommand(consulta, con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            List<EstudianteModel> listaEstudiantes = new List<EstudianteModel>();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                listaEstudiantes.Add(
+                    new EstudianteModel
+                    {
+                        carne = Convert.ToString(dr["carne"]),
+                        nombre = Convert.ToString(dr["nombre"]),
+                        apellido = Convert.ToString(dr["apellido"]),
+                        apellido2 = Convert.ToString(dr["apellido2"]),
+                    });
+            }
+            return listaEstudiantes;
+        }
+    }
+}
